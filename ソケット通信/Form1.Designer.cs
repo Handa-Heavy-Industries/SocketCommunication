51ecfd60e63e9a0d7f3e215dcf07483a0942bdf1
Shweta <119571578+kensa11@users.noreply.github.com>
2024-10-24 16:30:23 +0900

    Add project files.

---
+++ ソケット通信/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace ソケット通信
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txb_Port = new System.Windows.Forms.TextBox();
+            this.lbl_IPAddress = new System.Windows.Forms.Label();
+            this.lbl_Port = new System.Windows.Forms.Label();
+            this.lbl_Message = new System.Windows.Forms.Label();
+            this.lbl_ReceivedMsg = new System.Windows.Forms.Label();
+            this.txb_IPAddress = new System.Windows.Forms.TextBox();
+            this.btn_Connect = new System.Windows.Forms.Button();
+            this.lbl_ConnectionStatus = new System.Windows.Forms.Label();
+            this.txb_Message = new System.Windows.Forms.TextBox();
+            this.btn_SendMessage = new System.Windows.Forms.Button();
+            this.lst_ReceivedMsg = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnNextCommand = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txb_Port
+            // 
+            this.txb_Port.BackColor = System.Drawing.Color.Beige;
+            this.txb_Port.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txb_Port.Location = new System.Drawing.Point(122, 64);
+            this.txb_Port.Name = "txb_Port";
+            this.txb_Port.Size = new System.Drawing.Size(184, 22);
+            this.txb_Port.TabIndex = 6;
+            this.txb_Port.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txb_Port.TextChanged += new System.EventHandler(this.Txb_Port_TextChanged);
+            this.txb_Port.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txb_Port_KeyDown);
+            // 
+            // lbl_IPAddress
+            // 
+            this.lbl_IPAddress.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_IPAddress.Location = new System.Drawing.Point(16, 16);
+            this.lbl_IPAddress.Name = "lbl_IPAddress";
+            this.lbl_IPAddress.Size = new System.Drawing.Size(92, 30);
+            this.lbl_IPAddress.TabIndex = 0;
+            this.lbl_IPAddress.Text = "IPアドレス";
+            this.lbl_IPAddress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Port
+            // 
+            this.lbl_Port.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Port.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.lbl_Port.Location = new System.Drawing.Point(16, 60);
+            this.lbl_Port.Name = "lbl_Port";
+            this.lbl_Port.Size = new System.Drawing.Size(92, 30);
+            this.lbl_Port.TabIndex = 1;
+            this.lbl_Port.Text = "ポート";
+            this.lbl_Port.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_Message
+            // 
+            this.lbl_Message.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_Message.Location = new System.Drawing.Point(16, 145);
+            this.lbl_Message.Name = "lbl_Message";
+            this.lbl_Message.Size = new System.Drawing.Size(92, 30);
+            this.lbl_Message.TabIndex = 2;
+            this.lbl_Message.Text = "送信文字列";
+            this.lbl_Message.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_ReceivedMsg
+            // 
+            this.lbl_ReceivedMsg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbl_ReceivedMsg.Location = new System.Drawing.Point(16, 232);
+            this.lbl_ReceivedMsg.Name = "lbl_ReceivedMsg";
+            this.lbl_ReceivedMsg.Size = new System.Drawing.Size(92, 30);
+            this.lbl_ReceivedMsg.TabIndex = 4;
+            this.lbl_ReceivedMsg.Text = "受信文字列";
+            this.lbl_ReceivedMsg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txb_IPAddress
+            // 
+            this.txb_IPAddress.BackColor = System.Drawing.Color.Beige;
+            this.txb_IPAddress.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txb_IPAddress.Location = new System.Drawing.Point(122, 20);
+            this.txb_IPAddress.Name = "txb_IPAddress";
+            this.txb_IPAddress.Size = new System.Drawing.Size(184, 22);
+            this.txb_IPAddress.TabIndex = 5;
+            this.txb_IPAddress.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txb_IPAddress.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Txb_IPAddress_KeyDown);
+            // 
+            // btn_Connect
+            // 
+            this.btn_Connect.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_Connect.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btn_Connect.Location = new System.Drawing.Point(321, 16);
+            this.btn_Connect.Name = "btn_Connect";
+            this.btn_Connect.Size = new System.Drawing.Size(44, 74);
+            this.btn_Connect.TabIndex = 7;
+            this.btn_Connect.Text = "接続";
+            this.btn_Connect.UseVisualStyleBackColor = false;
+            this.btn_Connect.Click += new System.EventHandler(this.Btn_Connect_Click);
+            // 
+            // lbl_ConnectionStatus
+            // 
+            this.lbl_ConnectionStatus.Location = new System.Drawing.Point(16, 107);
+            this.lbl_ConnectionStatus.Name = "lbl_ConnectionStatus";
+            this.lbl_ConnectionStatus.Size = new System.Drawing.Size(349, 30);
+            this.lbl_ConnectionStatus.TabIndex = 8;
+            this.lbl_ConnectionStatus.Text = "------------------------  未接続  ------------------------";
+            this.lbl_ConnectionStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txb_Message
+            // 
+            this.txb_Message.BackColor = System.Drawing.Color.Beige;
+            this.txb_Message.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.txb_Message.Location = new System.Drawing.Point(122, 149);
+            this.txb_Message.Name = "txb_Message";
+            this.txb_Message.Size = new System.Drawing.Size(243, 22);
+            this.txb_Message.TabIndex = 9;
+            this.txb_Message.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btn_SendMessage
+            // 
+            this.btn_SendMessage.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btn_SendMessage.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_SendMessage.Location = new System.Drawing.Point(122, 179);
+            this.btn_SendMessage.Name = "btn_SendMessage";
+            this.btn_SendMessage.Size = new System.Drawing.Size(91, 34);
+            this.btn_SendMessage.TabIndex = 10;
+            this.btn_SendMessage.Text = "送信";
+            this.btn_SendMessage.UseVisualStyleBackColor = false;
+            this.btn_SendMessage.Click += new System.EventHandler(this.Btn_SendMessage_Click);
+            // 
+            // lst_ReceivedMsg
+            // 
+            this.lst_ReceivedMsg.BackColor = System.Drawing.Color.Beige;
+            this.lst_ReceivedMsg.FormattingEnabled = true;
+            this.lst_ReceivedMsg.ItemHeight = 12;
+            this.lst_ReceivedMsg.Location = new System.Drawing.Point(122, 232);
+            this.lst_ReceivedMsg.Name = "lst_ReceivedMsg";
+            this.lst_ReceivedMsg.ScrollAlwaysVisible = true;
+            this.lst_ReceivedMsg.Size = new System.Drawing.Size(243, 148);
+            this.lst_ReceivedMsg.TabIndex = 12;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(122, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "アプリ起動";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(274, 390);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 35);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "アプリ停止";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnNextCommand
+            // 
+            this.btnNextCommand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnNextCommand.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnNextCommand.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNextCommand.Font = new System.Drawing.Font("MS UI Gothic", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.btnNextCommand.Location = new System.Drawing.Point(274, 179);
+            this.btnNextCommand.Name = "btnNextCommand";
+            this.btnNextCommand.Size = new System.Drawing.Size(91, 34);
+            this.btnNextCommand.TabIndex = 14;
+            this.btnNextCommand.Text = " >>";
+            this.btnNextCommand.UseVisualStyleBackColor = false;
+            this.btnNextCommand.Click += new System.EventHandler(this.btnNextCommand_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(380, 437);
+            this.Controls.Add(this.btnNextCommand);
+            this.Controls.Add(this.lbl_ReceivedMsg);
+            this.Controls.Add(this.lbl_Message);
+            this.Controls.Add(this.lbl_Port);
+            this.Controls.Add(this.lbl_IPAddress);
+            this.Controls.Add(this.txb_Port);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_SendMessage);
+            this.Controls.Add(this.txb_IPAddress);
+            this.Controls.Add(this.lst_ReceivedMsg);
+            this.Controls.Add(this.txb_Message);
+            this.Controls.Add(this.btn_Connect);
+            this.Controls.Add(this.lbl_ConnectionStatus);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "PLC";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbl_IPAddress;
+        private System.Windows.Forms.Label lbl_Port;
+        private System.Windows.Forms.Label lbl_Message;
+        private System.Windows.Forms.Label lbl_ReceivedMsg;
+        private System.Windows.Forms.TextBox txb_IPAddress;
+        private System.Windows.Forms.TextBox txb_Port;
+        private System.Windows.Forms.Button btn_Connect;
+        private System.Windows.Forms.Label lbl_ConnectionStatus;
+        private System.Windows.Forms.TextBox txb_Message;
+        private System.Windows.Forms.Button btn_SendMessage;
+        private System.Windows.Forms.ListBox lst_ReceivedMsg;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnNextCommand;
+    }
+}
+
---
+++ ソケット通信/Form1.cs
@@ -0,0 +1,384 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ソケット通信
+{
+    /// <summary>
+    /// メインフォームクラス
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        int count = 0;
+
+        string ipAddress_ = "127.0.0.1"; // Define IP Address       
+        int portNumber_ = 11111;//Define Port Number
+        List<string> sendCommands = new List<string> {"Command1", "Command2", "...", "CommandN" }; //Define your PLC Commands Here
+
+
+        int sendCommandCount = 0;
+        int sndCmdIncrementor = 0;
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            //// ISCO Timer
+            //this.ISCOTimer = new System.Timers.Timer(4000);
+            //this.ISCOTimer.Elapsed += (sender, e) =>
+            //{
+            //    Console.WriteLine("ISCOTImer Elapsed..........");
+            //    SendMessage("ISCO");
+            //};
+
+            // ISCO Timer
+            this.InspectTimer = new System.Timers.Timer(3000);
+            this.InspectTimer.Elapsed += (sender, e) =>
+            {
+                count++;
+                Console.WriteLine("InspectTImer Elapsed..........");
+                SendMessage("INSPECT");
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txb_IPAddress.Text = ipAddress_.ToString();
+            txb_Port.Text = portNumber_.ToString();
+
+            sendCommandCount = sendCommands.Count();
+            txb_Message.Text = sendCommands[0].ToString();
+            Console.WriteLine("Command Count : {0}", sendCommandCount.ToString());
+        }
+
+        public TcpListener listener = null;
+        static NetworkStream networkStream = null;
+        byte[] bufferToRead;
+
+        //Timer to Send ISCO
+        private System.Timers.Timer ISCOTimer;
+
+        //Timer to Send INSPECT
+        private System.Timers.Timer InspectTimer;
+
+        //handler is the socket to handle the communication from client
+        public TcpClient handler = null;
+
+        /// <summary>
+        /// 接続ボタンのイベントハンドル
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_Connect_Click(object sender, EventArgs e)
+        {
+             
+            if (btn_Connect.Text == "接続")
+            {
+                // Define IP Address and Port Number
+                string ipAddressIn = ipAddress_;
+                //string ipAddressIn = "192.168.1.28";
+                int port = portNumber_;                
+
+                IPAddress ipAddress = System.Net.IPAddress.Parse(ipAddressIn);
+                //IPEndPoint host = new IPEndPoint(ipAddress, port);
+
+                // Display IP Address and Port Number on the Form
+                txb_IPAddress.Text = ipAddressIn;
+                txb_Port.Text = port.ToString();
+
+                // Create Listener's Socket
+                this.listener = new TcpListener(ipAddress, port);
+                this.listener.Start();
+
+                addToList("Listening....");
+
+                // Waiting for Connection from Client
+                this.listener.BeginAcceptTcpClient(new AsyncCallback(DoAcceptTcpClientCallback), listener);
+
+                // Update server connection status
+                btn_Connect.Text = "切断";
+                lbl_ConnectionStatus.Text = "接続済";                
+
+            }
+            else if(btn_Connect.Text == "切断")
+            {
+                //ISCOTimer.Stop();
+
+                InspectTimer.Stop();
+                 
+                // Stop listening and close handler's socket
+                this.listener.Stop();
+
+                if(this.handler != null)
+                {
+                    this.handler.Close();
+                }                
+
+                // Update Connection Status
+                btn_Connect.Text = "接続";
+                lbl_ConnectionStatus.Text = "未接続";
+            }
+        }
+        
+        /// <summary>
+        /// Accept TCP Client in Asynchronous Method
+        /// </summary>
+        /// <param name="ar"></param>
+        public void DoAcceptTcpClientCallback(IAsyncResult ar)
+        {
+            
+            TcpListener listener = (TcpListener)ar.AsyncState;
+
+            try
+            {
+                // End Asynchronous Task
+                handler = listener.EndAcceptTcpClient(ar);
+
+                // Display message that the client is connected
+                addToList("Client is Connected");
+
+                //ISCOTimer.Start();
+
+                
+                // Read from network stream
+                networkStream = handler.GetStream();
+
+                bufferToRead = new byte[1024];
+
+                networkStream.BeginRead(bufferToRead, 0, bufferToRead.Length,
+                                                 new AsyncCallback(ReadCallBack),
+                                                 networkStream);
+
+                //SendMessage("SET 10 20 30 40 50 60 70 80 90 100 110");
+
+            }
+            catch (ObjectDisposedException)
+            {
+                // Display message that the listening task has been stopped
+                addToList("Stop Listening..");
+            }
+            
+        }
+
+        int num = 0;
+        /// <summary>
+        /// ReadCallBack Function
+        /// </summary>
+        /// <param name="ar"></param>
+        public void ReadCallBack(IAsyncResult ar)
+        {
+            try
+            {
+                // End Asynchronous task
+                int len = networkStream.EndRead(ar);
+
+                if (len > 0)
+                {
+                    // ASCII decoding
+                    string result = Encoding.ASCII.GetString(bufferToRead);
+
+                    // Add the received message to the list
+                    if (result.StartsWith("OK"))
+                    {
+                        Console.WriteLine("Result received........");
+                        num++;
+                        addToList("OK " + num.ToString());
+                    }
+                    //else if (result.StartsWith("IRO"))
+                    //{
+                    //    //num++;
+                    //    addToList("IRO: " + result);
+                    //}
+                    else if (result.StartsWith("NG"))
+                    {
+                        num++;
+                        addToList("NG " + num.ToString());
+                    }
+                    else if(result.StartsWith("ISCO"))
+                    {
+                        SendMessage("OK");
+                        addToList(result);
+                    }
+                    else if(result.StartsWith("RESULT"))
+                    {
+                        addToList(result);
+                        Thread.Sleep(5000);
+
+                        count++;
+                        if(count < 50)
+                        {
+                            //SendMessage("START RUV168");
+                            //Thread.Sleep(2000);
+                            //SendMessage("INSPECT 0");
+                        }                        
+                    }
+                    else
+                    {
+                        addToList(result);
+                    }
+
+                    if(this.count == 7000)
+                    {
+                        this.InspectTimer.Stop();
+                        this.count = 0;
+                    }
+
+                    // Continue reading from the network stream
+                    bufferToRead = new byte[1024];
+                    networkStream.BeginRead(bufferToRead, 0, bufferToRead.Length,
+                                                     new AsyncCallback(ReadCallBack),
+                                                     networkStream);
+                }
+                else
+                {
+                    // Display Message that the client has been disconnected
+                    addToList("Client is disconnected");
+
+                    // Dispose network stream
+                    networkStream.Dispose();
+                    
+                    // Waiting for a new connection
+                    addToList("Listening..");
+                    this.listener.BeginAcceptTcpClient(new AsyncCallback(DoAcceptTcpClientCallback), listener);
+                }
+            }
+            catch
+            { }            
+        }
+ 
+        /// <summary>
+        /// 送信ボタンのイベントハンドル
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Btn_SendMessage_Click(object sender, EventArgs e)
+        {
+            SendMessage(txb_Message.Text);
+
+        }
+
+        private void SendMessage(string msgIn)
+        {
+            // Get string message from textbox
+            string msg = msgIn + "\r\n";
+
+            if (this.InvokeRequired)
+            {
+                this.BeginInvoke(new Action(() => this.txb_Message.Text = msg));
+            }
+            else
+            {
+                this.txb_Message.Text = msg;
+            }            
+
+            // ASCII encoding
+            byte[] bufferToWrite = Encoding.ASCII.GetBytes(msg);
+
+            // Write to network stream
+            if (networkStream != null && networkStream.CanWrite)
+            {
+                try
+                {
+                    networkStream.BeginWrite(bufferToWrite, 0, bufferToWrite.Length,
+                    new AsyncCallback(WriteCallBack), networkStream);
+                }
+                catch
+                {
+                    addToList("Connection Problem, Close Handler's Socket!");
+                    this.handler.Close();
+                    this.handler = null;
+                    this.listener.BeginAcceptTcpClient(new AsyncCallback(DoAcceptTcpClientCallback), listener);
+                }
+            }
+            else
+            {
+                addToList("Can't Write to the Network Stream..");
+            }
+        }
+
+        /// <summary>
+        /// WriteCallBack Function
+        /// </summary>
+        /// <param name="ar"></param>
+        public void WriteCallBack(IAsyncResult ar)
+        {
+            // End Asynchronous task
+            networkStream.EndWrite(ar);
+        }
+
+        /// <summary>
+        /// Write to the ListBox
+        /// </summary>
+        /// <param name="result"></param>
+        private void addToList(string result)
+        {
+            if (lst_ReceivedMsg.InvokeRequired)
+            {
+                this.Invoke((MethodInvoker)(() => this.lst_ReceivedMsg.Items.Add(result)));
+            }
+            else
+            {
+                lst_ReceivedMsg.Items.Add(result);
+            }
+        }
+
+
+        private void Txb_IPAddress_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                txb_Port.Focus();
+            }
+        }
+
+        private void Txb_Port_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Txb_Port_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                btn_Connect.Focus();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InspectTimer.Start();
+
+            //Send START Command
+            SendMessage("START");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            InspectTimer.Stop();
+            
+            SendMessage("STOP");
+        }
+
+        private void btnNextCommand_Click(object sender, EventArgs e)
+        {
+            //sndCmdIncrementor++;
+            Console.WriteLine("sndCmdInc : {0}", sndCmdIncrementor.ToString());
+            sndCmdIncrementor = (sndCmdIncrementor == sendCommandCount-1) ? 1 : sndCmdIncrementor + 1;            
+            txb_Message.Text = sendCommands[sndCmdIncrementor];
+            SendMessage(txb_Message.Text);
+        }
+    }
+}
